815668b2ab8d419b2c4ed80476a30bb7987b1a49
andrea <andrea.angelova@hotmail.com>
2019-04-20 19:13:39 +0200

    setting up modules and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/customers', pathMatch: 'full'},
+  {path: 'customers', loadChildren: './modules/customer/customer.module#CustomerModule'},
+  {path: 'products', loadChildren: './modules/product/product.module#ProductModule'},
+  {path: '**', redirectTo: '/customers'} // PageNotFound
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
